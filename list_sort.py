d8f155fd4d3fcf711d9c7931942113a36c42c90e
clareaduett <42472762+clareaduett@users.noreply.github.com>
2018-09-17 14:38:55 +0300

    Create list_sort.py

---
+++ list_sort.py
@@ -0,0 +1,33 @@
+#List
+list=[X,Y,Z]
+
+#Defining the variables
+X=X.extend(int())
+Y=Y.extend(float())
+Z=''
+
+#List for sorting
+list.sort[X,Y,Z]
+
+#Return
+my_dict={evens:even_numbers,odds:odd_numbers,chars:''}
+
+for even_numbers in X 
+         if even_numbers%2==0:
+             return(isinstance(even_numbers,int)
+                    else:
+                    return()
+for odd_numbers in X and Y
+                    if odd_numbers%2==1:
+                    return(isinstance(odd_numbers,int or float)
+                           else:
+                           return()
+for chars in Z                           
+                           if chars==str:
+                           return(isinstance(chars,str)
+                                  else:
+                                  return()
+    
+d={evens:[sorted(even_numbers)],odds:sorted[(odd_numbers)],chars:[sorted('')]}
+
+ print(d)   
